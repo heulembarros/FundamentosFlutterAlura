e27b499020514f388df348ae287d67ac94538403
Heulem Barros <heulembarros@gmail.com>
2022-02-10 17:38:54 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_back),
+          title: Text('Criando Transferências'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: TextEditingController(),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                    labelText: 'Número da Conta', hintText: '00000-00'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor da Transfrência',
+                    hintText: '0.00'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              color: Colors.deepPurple,
+              child: Text('Confirmar'),
+              onPressed: () {
+                debugPrint('CONFIRMADO');
+              },
+            )
+          ],
+        ));
+  }
+}
+
+class ListaTranferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Icon(Icons.menu_rounded),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+        title: Text('Transferências'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(200.00, '2000-00')),
+          ItemTransferencia(Transferencia(300.00, '3000-00')),
+          ItemTransferencia(Transferencia(400.00, '4000-00')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+              ),
+              child: Text(
+                'ByteBank',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Mensagens'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Perfil'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configurações'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta),
+        trailing: Icon(Icons.more_vert),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final String numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
